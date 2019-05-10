6cddb45e0b59c3ca0b02f30004df9c96cf93de22
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-10 14:51:52 +0200

    Wrote the code to calculate Euro to Yen and added some layout with bootstrap

---
+++ currencyexchange.js
@@ -0,0 +1,4 @@
+function curEx(){
+    var initialvalue=document.getElementById("input").value;
+    document.getElementById("output").innerHTML = "You can exchange €" + initialvalue + " for " + initialvalue*123.235 + " Yen";
+}
